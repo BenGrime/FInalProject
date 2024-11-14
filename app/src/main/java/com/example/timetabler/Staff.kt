d36aa87fd79b8dbe7648d96021aabe8b0da86d22
BenGrime37 <108684658+BenGrime37@users.noreply.github.com>
2024-11-14 13:01:14 +0000

    Staff object made, and design of home screen almost made

---
+++ app/src/main/java/com/example/timetabler/Staff.kt
@@ -0,0 +1,12 @@
+package com.example.timetabler
+
+import com.google.firebase.Timestamp
+import java.io.Serializable
+
+data class Staff(
+    var Id: String = "",
+    var Name: String = "",
+    var PreviousRide: String = "",
+    var DoB: Timestamp,
+    var RidesTrained: ArrayList<String>
+) : Serializable
