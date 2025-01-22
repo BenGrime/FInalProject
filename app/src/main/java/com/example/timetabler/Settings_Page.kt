142c1c92c32dcf2e0be97481f3de892b96d45fd8
BenGrime37 <108684658+BenGrime37@users.noreply.github.com>
2025-01-22 13:14:50 +0000

    settings page ui done for now

--- app/src/main/java/com/example/timetabler/Settings_Page.kt
+++ app/src/main/java/com/example/timetabler/Settings_Page.kt
@@ -2,10 +2,35 @@ package com.example.timetabler
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.ToggleButton
+import androidx.appcompat.app.ActionBarDrawerToggle
 
 class Settings_Page : AppCompatActivity() {
+
+    private lateinit var toggle1: ToggleButton
+    private lateinit var toggle2: ToggleButton
+    private lateinit var toggle3: ToggleButton
+    private lateinit var priorityList : TextView
+    private lateinit var deleteData : LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings_page)
+
+        toggle1 = findViewById(R.id.notifToggle)
+        toggle2 = findViewById(R.id.darkModeToggle)
+        toggle3 = findViewById(R.id.languageToggle)
+        deleteData = findViewById(R.id.deleteStaffData)
+        priorityList = findViewById(R.id.priorityListBtn)
+        deleteData.setOnClickListener{
+            //do stuff
+        }
+        priorityList.setOnClickListener{
+            //do something
+        }
+
+
+
     }
 }
